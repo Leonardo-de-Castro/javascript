ec568075e788d8657275cb36ec6ffcb4be21e312
Leonardo de Castro <127856638+Leonardo-de-Castro@users.noreply.github.com>
2023-06-13 16:02:36 -0300

    teste de REPETIÇÃO

---
+++ exercicios/ex014/ambienteteste.js
@@ -0,0 +1,26 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`) 
+    c++ // c++ Significa c = c + 1
+} while (c <= 20)
+
+
+/*
+var c = 1
+while (c <= 20) {
+    console.log(`Passo ${c}`) 
+    c++ // c++ Significa c = c + 1
+
+*/
+
+
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
